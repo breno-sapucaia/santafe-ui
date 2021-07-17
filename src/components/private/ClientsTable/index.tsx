e439f285a01dde1a3d646949b49b17e062c5d9f8
Carmichael Ferreira <carmichaelferreira06@gmail.com>
2021-07-17 12:07:07 -0300

    chore: re-structuring the ClientsTable Component

--- src/components/private/ClientsTable/index.tsx
+++ src/components/private/ClientsTable/index.tsx
@@ -1,7 +1,14 @@
-import { DataGrid, GridColDef } from '@material-ui/data-grid'
-import { useEffect, useState } from 'react'
+import { TablePagination } from '@material-ui/core'
+import {
+  DataGrid,
+  GridColDef,
+  GridEditRowModelParams,
+  GridRowId,
+  GridSelectionModelChangeParams,
+  useGridSlotComponentProps,
+} from '@material-ui/data-grid'
+import { useCallback, useEffect, useState } from 'react'
 import { clientsData } from '../../../mock'
-
 interface ClientsTable {
   searchTerm: string
 }
@@ -31,7 +38,21 @@ export default function ClientsTable({ searchTerm }: ClientsTable) {
       editable: true,
     },
   ]
+  const [selectionModel, setSelectionModel] = useState<GridRowId[]>([])
   const [rowsData, setRowsData] = useState(clientsData)
+
+  const handleRowSelection = ({
+    selectionModel,
+  }: GridSelectionModelChangeParams) => {
+    setSelectionModel(selectionModel)
+  }
+
+  const handleEditRowModelChange = useCallback(
+    ({ model }: GridEditRowModelParams) => {
+      console.log('model: ', model)
+    },
+    []
+  )
 
   const filterRows = () => {
     setRowsData(
@@ -45,13 +66,36 @@ export default function ClientsTable({ searchTerm }: ClientsTable) {
     filterRows()
   }, [searchTerm])
 
+  const CustomPagination = () => {
+    const {
+      state: { pagination },
+      apiRef,
+    } = useGridSlotComponentProps()
+    console.log('pagination: ', pagination)
+    return (
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 50, 100]}
+        count={pagination.pageCount}
+        rowsPerPage={10}
+        page={pagination.page}
+        onChangePage={(event, value) => apiRef.current.setPage(value - 1)}
+      />
+    )
+  }
+
   return (
     <div style={{ height: '80vh', width: '100%', marginTop: '10px' }}>
       <DataGrid
+        onSelectionModelChange={handleRowSelection}
+        onEditRowModelChange={handleEditRowModelChange}
         rows={rowsData}
         columns={columns}
         checkboxSelection
         disableSelectionOnClick
+        rowsPerPageOptions={[10, 25, 50, 100]}
+        components={{
+          Pagination: CustomPagination,
+        }}
       />
     </div>
   )
